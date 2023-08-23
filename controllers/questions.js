e496c1e707a869ee8701e585f37b547a88b38324
Smith Ringtho <sringtho@gmail.com>
2023-08-23 16:38:36 +0300

    Added the questions endpoints functionality

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,23 +1,47 @@
+const Question = require('../models/question')
 const { StatusCodes } = require('http-status-codes')
+const { NotFoundError } = require('../errors')
 
 const getAllQuestions = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'All Questions' })
+  const questions = await Question.find({ })
+  res.status(StatusCodes.OK).json({ questions, count: questions.length })
 }
 
 const createQuestion = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'Create Questions' })
+  const question = await Question.create(req.body)
+  res.status(StatusCodes.CREATED).json({ question })
 }
 
 const getSingleQuestion = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Get Question' })
+  const questionId = req.params.id
+  const question = await Question.findOne({ _id: questionId })
+  if (!question) {
+    throw new NotFoundError(`Question with id ${questionId} does not exist`)
+  }
+  res.status(StatusCodes.OK).json({ question })
 }
 
 const updateQuestion = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Update Question' })
+  const questionId = req.params.id
+  const question = await Question.findOneAndUpdate({ _id: questionId },
+    req.body,
+    { new: true, runValidators: true }
+  )
+  if (!question) {
+    throw new NotFoundError(`Question with id ${questionId} does not exist`)
+  }
+  res.status(StatusCodes.OK).json({ question })
 }
 
 const deleteQuestion = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Get Question' })
+  const questionId = req.params.id
+  const question = await Question.findOneAndRemove({ _id: questionId })
+  if (!question) {
+    throw new NotFoundError(`Question with id ${questionId} does not exist`)
+  }
+  res.status(StatusCodes.OK).json({
+    msg: `Successfully deleted question with id ${questionId}`
+  })
 }
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -17,7 +17,8 @@ const userSchema = new Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please provide a valid email'
     ],
-    unique: true
+    unique: true,
+    lowercase: true
   },
   password: {
     type: String,
